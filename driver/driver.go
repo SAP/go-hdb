349bc839b548ff3658aa7d9dd01fde2929e26a0b
Stefan Miller <d024441@users.noreply.github.com>
2016-07-04 09:46:32 +0200

    release 0.8

--- driver/driver.go
+++ driver/driver.go
@@ -33,7 +33,7 @@ import (
 )
 
 // DriverVersion is the version number of the hdb driver.
-const DriverVersion = "0.2.1"
+const DriverVersion = "0.8"
 
 // DriverName is the driver name to use with sql.Open for hdb databases.
 const DriverName = "hdb"
