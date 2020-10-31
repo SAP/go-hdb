d7bfdac24beb2491e9302515315d3f3b62ed847f
d024441 <stefan.miller@sap.com>
2020-10-31 14:13:28 +0100

    added fixed

--- driver/connector.go
+++ driver/connector.go
@@ -37,11 +37,12 @@ const (
 	DfvLevel8 = 8 // with FIXED8/12/16 support
 )
 
-var supportedDfvs = map[int]bool{DfvLevel1: true, DfvLevel4: true, DfvLevel6: true, DfvLevel8: true}
+//var supportedDfvs = map[int]bool{DfvLevel1: true, DfvLevel4: true, DfvLevel6: true, DfvLevel8: true}
+var supportedDfvs = map[int]bool{DfvLevel1: true, DfvLevel4: true, DfvLevel6: true}
 
 // Connector default values.
 const (
-	DefaultDfv          = DfvLevel8         // Default data version format level.
+	DefaultDfv          = DfvLevel6         // Default data version format level.
 	DefaultTimeout      = 300 * time.Second // Default value connection timeout (300 seconds = 5 minutes).
 	DefaultTCPKeepAlive = 15 * time.Second  // Default TCP keep-alive value (copied from net.dial.go)
 	DefaultBufferSize   = 16276             // Default value bufferSize.
